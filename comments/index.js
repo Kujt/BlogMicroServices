c889ab491d69ba71b2150faf1365a3685fd9fce3
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-07-13 21:31:42 +0200

    Create Comment Service
    implement the comment service in backend

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+app.post("/posts/:id/comments", (req, res) => {
+  // create a random Id
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  //   This will create an array if posts exist otherwise empty array
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening in port 4001");
+});
