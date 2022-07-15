d89f4fc84c2b9fa0c9ac781846f907c1b2142171
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-07-15 21:15:51 +0200

    Change in client
    Getting all the post from query service

--- client/src/CommentList/CommentList.js
+++ client/src/CommentList/CommentList.js
@@ -1,20 +1,6 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
+import React from "react";
 
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-    // eslint-disable-next-line
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
