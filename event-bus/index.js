220709f37110a7af9d855de212b02db5e25d3ad9
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-07-16 23:00:17 +0200

    Fixing some bugs

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,16 +7,32 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
+
   // Down side we are asuming all events are going to succed
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
   res.send({ status: "OK" });
 });
 
+// endpoint to retrive all events that ever ocourd
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 app.listen(4005, () => {
   console.log("Listening on 4005");
 });
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,14 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-// this is endpoint that is going to receive events from our event bus
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
@@ -27,13 +21,33 @@ app.post("/events", (req, res) => {
     const post = posts[postId];
     post.comments.push({ id, content });
   }
+};
 
-  console.log(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+// this is endpoint that is going to receive events from our event bus
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
   // all function it is a rout handler and we need to send back some kind of response
   //   we are sending a empty object just to confirm that we received event
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
