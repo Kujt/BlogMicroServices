d5153f1808261cdf76906d04a9c3fe1e11b8c050
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-07-28 12:47:59 +0200

    Making changes in client

--- client/src/PostList/PostList.js
+++ client/src/PostList/PostList.js
@@ -8,7 +8,7 @@ const PostList = () => {
 
   //   Function that are going to make a request to our post service
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:4002/posts");
+    const res = await axios.get("http://posts.com/posts");
 
     setPosts(res.data);
   };
